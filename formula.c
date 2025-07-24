1d3e09ae7fa51666238e6f62d546fcc6e430b372
Muppur Gnaneswar <muppurgnaneswar@gmail.com>
2025-07-24 18:27:55 +0530

    Update formula.c

--- formula.c
+++ formula.c
@@ -1,50 +1,61 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
-    int i,l,h,r;
+    int i, l, h, r;
     float area;
     float volume;
-   printf("Select the shape:-\n1.Cube\n2.Sphere\n3.Cylinder\n4.Cone\n\nEnter the number:");
-   scanf("%d", &i);
-   switch(i){
-            case 1:  printf("r=");
-                     scanf("%d", &r);
-               area=6*r*r;
-               volume=r*r*r;
-            printf("Area of a Cube=%f\n", area);
-            printf("Volume of a Cube=%f", volume);
+
+    printf("Select the shape:-\n1. Cube\n2. Sphere\n3. Cylinder\n4. Cone\n\nEnter the number: ");
+    scanf("%d", &i);
+
+    switch(i) {
+        case 1:
+            printf("Enter side length (r) of the Cube: ");
+            scanf("%d", &r);
+            area = 6 * r * r;
+            volume = r * r * r;
+            printf("Surface Area of the Cube = %.2f\n", area);
+            printf("Volume of the Cube = %.2f\n", volume);
             break;
-            case 2:  printf("r=");
-                     scanf("%d", &r);
-               area=4*3.14*r*r;
-               volume=1.33*3.14*r*r*r;
-            printf("Area of a Sphere=%f\n", area);
-            printf("Volume of a Sphere=%f", volume);
+
+        case 2:
+            printf("Enter radius (r) of the Sphere: ");
+            scanf("%d", &r);
+            area = 4 * M_PI * r * r;
+            volume = (4.0 / 3.0) * M_PI * r * r * r;
+            printf("Surface Area of the Sphere = %.2f\n", area);
+            printf("Volume of the Sphere = %.2f\n", volume);
             break;
-            case 3:  printf("r=");
-                     scanf("%d", &r);
-                     printf("h=");
-                     scanf("%d", &h);
-   
-               area=2*3.14*r*(r+h);
-               volume=3.14*r*r*h;
-            printf("Area of a Cylinder=%f\n", area);
-            printf("Volume of a Cylinder=%f", volume);
+
+        case 3:
+            printf("Enter radius (r) of the Cylinder: ");
+            scanf("%d", &r);
+            printf("Enter height (h) of the Cylinder: ");
+            scanf("%d", &h);
+            area = 2 * M_PI * r * (r + h);
+            volume = M_PI * r * r * h;
+            printf("Surface Area of the Cylinder = %.2f\n", area);
+            printf("Volume of the Cylinder = %.2f\n", volume);
             break;
-            case 4: printf("r=");
-                    scanf("%d", &r);
-                    printf("h=");
-                    scanf("%d", &h);
-                    printf("l=");
-                    scanf("%d", &l);
-               area=3.14*r*(r+l);
-               volume=0.33*3.14*r*r*h;
-            printf("Area of a Cone=%f\n", area);
-            printf("Volume of a Cone=%f", volume);
+
+        case 4: 
+            printf("Enter radius (r) of the Cone: ");
+            scanf("%d", &r);
+            printf("Enter height (h) of the Cone: ");
+            scanf("%d", &h);
+            printf("Enter slant height (l) of the Cone: ");
+            scanf("%d", &l);
+            area = M_PI * r * (r + l);
+            volume = (1.0 / 3.0) * M_PI * r * r * h;
+            printf("Surface Area of the Cone = %.2f\n", area);
+            printf("Volume of the Cone = %.2f\n", volume);
             break;
-            default:
-            printf("Invalid selection");
+
+        default:
+            printf("Invalid selection.\n");
             break;
-   }
-   return 0;
+    }
+
+    return 0;
 }
